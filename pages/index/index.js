cf8b8d4c8c805b5e65944ab61b04ca06c52b2391
www-wpx <1073789172@qq.com>
2018-09-18 22:53:03 +0800

    改
    老公

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello, 小仙女',
+    motto: 'Hello, 老公',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
